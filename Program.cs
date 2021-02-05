ca4a7fc84ca9d90ebd06754067e4bdf209939e09
twinbird827 <twinbird827@gmail.com>
2021-02-05 15:41:54 +0900

    zip圧縮ﾃｽﾄ

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using My.Core;
+using My.Core.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) return;
+
+            var dir = args[0];
+
+            if (!Directory.Exists(dir)) return;
+
+            CoreUtil.CreateZipFromDirectory(dir, $"{dir}.zip");
+        }
+    }
+}
